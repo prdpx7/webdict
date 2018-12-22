62b8b1121c4be7b51a4dab21d41befd11da600bb
Daniel Schildt <daniel.schildt@gmail.com>
2018-12-22 03:17:13 +0200

    fix(index.js): update data sources + reformat with eslint
    - Update data source URL from HTTP to HTTPS (to avoid various redirects & issues)
    - Reformat source code with `eslint-fix`
    - Improve readability of the code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const cheerio = require('cheerio')
 const got = require('got')
 
-const dictionaryURL = 'http://dictionary.com/browse/'
-const urbanDictionaryURL = 'http://api.urbandictionary.com/v0/define?term='
+const dictionaryURL = 'https://www.dictionary.com/browse/'
+const urbanDictionaryURL = 'https://api.urbandictionary.com/v0/define?term='
 
 function getURL(site, word) {
   if (site === 'dictionary') {
     return dictionaryURL + word
-  } else if (site === 'urbandictionary') {
+  } if (site === 'urbandictionary') {
     return urbanDictionaryURL + word
   }
 
@@ -31,57 +31,53 @@ function urbanDictionaryScrapper(jsonBody) {
       message: 'success',
       type: 'Unknown',
       definition: json.list.map(item => item.definition).slice(0, 3),
-      source: 'http://urbandictionary.com'
+      source: 'https://www.urbandictionary.com'
     }
   }
 
   return {
     statusCode: 404,
     message: 'check the spelling again',
-    source: 'http://urbandictionary.com'
+    source: 'https://www.urbandictionary.com'
   }
 }
 
 function dictionaryScrapper(htmlBody) {
   const $ = cheerio.load(htmlBody)
-  const definitionList =
-    $('div section div div section div .def-content')
-      .text()
-      .split('\r\n')
+  // Website has dynamic CSS class names, making it difficult to match the right elements.
+  const definitionList = $('main section section .expand-container .css-2oywg7')
+    .text()
+    .split('\r\n')
+    .map(stripping)
+    .filter(isWord)
+    .map(val => val
+      .replace('(', '')
+      .replace(')', '')
+      .split(':')
       .map(stripping)
       .filter(isWord)
-      .map(val =>
-        val
-          .replace('(', '')
-          .replace(')', '')
-          .split(':')
-          .map(stripping)
-          .filter(isWord)
-          .join(':')
-      )
-      .slice(0, 3)
+      .join(':')
+    )
+    .slice(0, 3)
 
   return {
     statusCode: '200',
     message: 'success',
     type: $('div section div div section header span').html(),
     definition: definitionList,
-    source: 'http://dictionary.com'
+    source: 'https://www.dictionary.com'
   }
 }
 
-const webdict = (site, word) =>
-  got(getURL(site, word.toLowerCase()))
-    .then(response =>
-      (site === 'dictionary' ? dictionaryScrapper : urbanDictionaryScrapper)(response.body)
-    )
-    .catch(err =>
-      ({
-        statusCode: err.statusCode,
-        message: 'Unable to fetch or check the source_url/spelling again',
-        source: `http://${err.host}`
-      })
-    )
+const webdict = (site, word) => got(getURL(site, word.toLowerCase()))
+  .then(response => (site === 'dictionary' ? dictionaryScrapper : urbanDictionaryScrapper)(response.body)
+  )
+  .catch(err => ({
+    statusCode: err.statusCode,
+    message: 'Unable to fetch or check the source_url/spelling again',
+    source: `https://${err.host}`
+  })
+  )
 
 
 module.exports = webdict
